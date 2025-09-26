4685226c47d8f82dcc31f1d645a3dfa4b855ea08
Itoyama424 <itoshige44524452@docomo.ne.jp>
2025-09-27 02:10:46 +0900

    page方式のレイアウト

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Component } from "react";
+import Lyout from './layout'
 
+// 引数で渡されたComponentとpagePropsというものを使い
+// Component {...pagePropes}という風に、呼び出されるページのコンポーネントとプロパティが渡される
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return(
+    <Lyout>
+    <Component {...pageProps} />;
+    </Lyout>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import Link from 'next/link'
+
+const inter = Inter({ subsets:['latin']})
 
 export default function Home() {
   return (
     <main>
-      <h1>SSG application.</h1>
       <p>This is sample page.</p>
       <div><Link href="/other">Go "Other".</Link></div>
     </main>
