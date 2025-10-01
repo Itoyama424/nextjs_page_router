23d788e43d68fba118319d228bed0a3efc02d6bc
Itoyama424 <itoshige44524452@docomo.ne.jp>
2025-10-02 00:33:02 +0900

    getStaticProps　192Pまで

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Component } from "react";
-import Lyout from './layout'
+import Layout from './layout'
 
+  const data = {
+    title: "Next Application",
+    msg: "My Website"
+  };
 // 引数で渡されたComponentとpagePropsというものを使い
-// Component {...pagePropes}という風に、呼び出されるページのコンポーネントとプロパティが渡される
+// Component {...pageProps}という風に、呼び出されるページのコンポーネントとプロパティが渡される
 export default function App({ Component, pageProps }: AppProps) {
   return(
-    <Lyout {...pageProps}>
+    <Layout {...pageProps}>
     <Component {...pageProps} />
-    </Lyout>
+    </Layout>
   )
 }
--- src/pages/other.tsx
+++ src/pages/other.tsx
@@ -1,12 +1,25 @@
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
+import  { GetStaticProps, GetStaticPropsContext } from 'next'
+import {PageData, props} from '../types/types'
+
 const inter = Inter({subsets:['latin']})
 
-export default function Other() {
-    return(
+export const getStaticProps: GetStaticProps<props> = async (context: GetStaticPropsContext) => {
+    const data = {
+        title:"Other page.",
+        msg:"これは静的プロパティのサンプルです。"
+    }
+    return {
+        props: {
+            data: data
+        }
+    }
+}
+export default function Other({ data }: props) {
+    return (
         <main className={inter.className}>
-            <h1>Other page.</h1>
-            <p>これは別のページです。</p>
+            <p>{data.msg}</p>
             <div><Link href="/">Go Back!!</Link></div>
         </main>
     )
